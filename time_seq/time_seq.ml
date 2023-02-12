e41744438410811d06f808506df9f1619d4b285d
Andrew Tao <aytao@princeton.edu>
2023-02-12 02:21:45 -0500

    Reduce timing test

--- time_seq/time_seq.ml
+++ time_seq/time_seq.ml
@@ -27,16 +27,19 @@ let gen_weak ():int Weak.t = Weak.create n;;
 let gen_norm () = Array.make n 0;;
 let gen_float () = Array.create_float n;;
 
+let test (): int =
+  ParallelSeq.reduce (+) 0 (gen_int_seq n)
+
 (* let s = gen_int_seq n *)
-let _ = print_endline (string_of_float (time (fun _ -> ignore (gen_weak ())) 10))
-let _ = print_endline (string_of_float (time (fun _ -> ignore (gen_norm ())) 10))
-let _ = print_endline (string_of_float (time (fun _ -> ignore (gen_float ())) 10))
+let _ = print_endline (string_of_float (time (fun _ -> ignore (test ())) 10))
+(* let _ = print_endline (string_of_float (time (fun _ -> ignore (gen_norm ())) 10))
+let _ = print_endline (string_of_float (time (fun _ -> ignore (gen_float ())) 10)) *)
 
 (* let _ = print_int (Obj.size (Obj.repr (gen_float ()))) *)
 
-let n_arr:nativeint array = Obj.magic (gen_norm ())
+(* let n_arr:nativeint array = Obj.magic (gen_norm ())
 
 print_endline "Hi"
 let i:int = Nativeint.to_int n_arr.(0)
 
-let _ = print_int i
+let _ = print_int i *)
