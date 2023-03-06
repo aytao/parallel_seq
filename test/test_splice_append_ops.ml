8dfc2b15fc0ee5a0abbdf3c034a63469e9b4ad95
Andrew Tao <aytao@princeton.edu>
2023-03-06 02:29:18 -0500

    Split test

--- test/test_splice_append_ops.ml
+++ test/test_splice_append_ops.ml
@@ -1,19 +1,18 @@
 open Seq
 open Sequence
 
-let n: int = (Defines.sequential_cutoff * 4) + 1;;
+let n: int = (Defines.sequential_cutoff * Defines.num_domains) + 1;;
 let num_trials: int = 10
 
 let test_split n =
   let s = S.tabulate (fun i -> i) n in
-  let s_lr = S.tabulate (fun i -> S.split s i) (n + 1) in
   
   let check_split idx: unit =
     let check_section section offset: unit =
       let _ = S.tabulate (fun idx -> assert (idx + offset = S.nth section idx)) (S.length section) in
       ()
     in
-    let l,r = S.nth s_lr idx in
+    let l,r = S.split s idx in
     assert (S.length l = idx);
     assert (S.length r = n - idx);
     check_section l 0;
