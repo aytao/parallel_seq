c494fcc26e6ace6a04af823a0e0117a25cce7da2
Andrew Tao <aytao@princeton.edu>
2023-04-08 17:04:17 -0400

    Python formatting

--- run_test.py
+++ run_test.py
@@ -1,5 +1,7 @@
-import os, sys
+import os
+import sys
 from sys import argv, exit
+
 
 def main():
   if len(argv) < 3:
@@ -15,26 +17,28 @@ def main():
 
   os.system("dune build")
 
-  print("Running '%s' sequentially..." % test_name, file = sys.stderr, end = "")
+  print("Running '%s' sequentially..." % test_name, file=sys.stderr, end="")
   sys.stderr.flush()
-  
+
   os.system(command + " -f")
 
-  print("Done", file = sys.stderr)
+  print("Done", file=sys.stderr)
   sys.stderr.flush()
 
   affinity_mask = {0}
   for i in range(1, max_num_domains + 1):
     os.sched_setaffinity(0, affinity_mask)
-    
-    print("Running '%s' with %d domain(s)..." % (test_name, i), file = sys.stderr, end = "")
+
+    print("Running '%s' with %d domain(s)..." %
+          (test_name, i), file=sys.stderr, end="")
     sys.stderr.flush()
 
     os.system(command)
 
-    print("Done", file = sys.stderr)
+    print("Done", file=sys.stderr)
     sys.stderr.flush()
     affinity_mask.add(i)
+
 
 if __name__ == "__main__":
   main()
