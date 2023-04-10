d5d33fd3dd4f4629c87ea9aeb34242cb4a4f38e7
Andrew Y. Tao <aytao@rinse.cs.princeton.edu>
2023-04-09 20:56:16 -0400

    Make timing file larger

--- time/time_index.ml
+++ time/time_index.ml
@@ -5,6 +5,6 @@ open Index
 let _ = S.empty ()
 
 let index, elapsed_time =
-  Time_utils.time Inverted_index.make_index "index_data/test_index_100000.txt"
+  Time_utils.time Inverted_index.make_index "index_data/test_index_2000000.txt"
 
 let _ = Printf.printf "%f\n" elapsed_time
