595e9032fe517a3c58caff43870401f0fb748c11
Andrew Tao <aytao@princeton.edu>
2023-04-11 22:47:24 -0400

    Index timing

--- time/time_index.ml
+++ time/time_index.ml
@@ -4,11 +4,18 @@ open Index
 
 let _ = S.empty ()
 
-let ii, parse_elapsed_time =
-  Time_utils.time Inverted_index.parse "index_data/test_index_2000000.txt"
+(*
+   let ii, parse_elapsed_time =
+     Time_utils.time Inverted_index.parse "index_data/test_index_1000000.txt"
 
-let index, combine_elapsed_time = Time_utils.time Inverted_index.combine ii
+   let index, combine_elapsed_time = Time_utils.time Inverted_index.combine ii
 
-let _ =
-  Printf.printf "Parse: %f, Combine: %f\n" parse_elapsed_time
-    combine_elapsed_time
+   let _ =
+     Printf.printf "Parse: %f, Combine: %f\n" parse_elapsed_time
+       combine_elapsed_time
+*)
+
+let index, elapsed_time =
+  Time_utils.time Inverted_index.make_index "index_data/test_index_1000000.txt"
+
+let _ = Printf.printf "%f\n" elapsed_time
