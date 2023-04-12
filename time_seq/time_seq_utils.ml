93713d2f3f44b8f4d703e36a3c6570aeedfdd469
Andrew Tao <aytao@princeton.edu>
2023-04-11 23:34:13 -0400

    Remove unused executable time_seq

--- time_seq/time_seq_utils.ml
+++
@@ -1,7 +0,0 @@
-let time (f : unit -> unit) (num_trials : int) : float =
-  let start = Unix.time () in
-  for i = 1 to num_trials do
-    f ()
-  done;
-  let elapsed = Unix.time () -. start in
-  elapsed /. float_of_int num_trials
