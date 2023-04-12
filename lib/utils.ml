f98a039c93c15788216fdc0f7a278b2d5371b94b
Andrew Tao <aytao@princeton.edu>
2023-04-12 01:35:13 -0400

    Change Utils to functor

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,4 +1,14 @@
 open Sequence
 
-let print_sequence (to_string : 'a -> string) (s : 'a S.t) : unit =
-  S.iter (fun i -> print_string (to_string i ^ ",")) s
+module type Utils = sig
+  type 'a s
+
+  val print_sequence : ('a -> string) -> 'a s -> unit
+end
+
+module Make (S : S) : Utils with type 'a s = 'a S.t = struct
+  type 'a s = 'a S.t
+
+  let print_sequence (to_string : 'a -> string) (s : 'a s) : unit =
+    S.iter (fun i -> print_string (to_string i ^ ",")) s
+end
