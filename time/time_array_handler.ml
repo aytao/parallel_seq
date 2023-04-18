c3e22bc00039953b12f782739ba3e9c332f56599
Andrew Y. Tao <aytao@rinse.cs.princeton.edu>
2023-04-18 00:08:22 -0400

    modify size of array

--- time/time_array_handler.ml
+++ time/time_array_handler.ml
@@ -2,7 +2,7 @@ open Seq
 open Sequence
 open Array_handler
 
-let n = Sys.max_array_length
+let n = 1000000000;;
 let memset_init n : int array = get_uninitialized n
 let _, elapsed_time = Time_utils.time memset_init n;;
 
