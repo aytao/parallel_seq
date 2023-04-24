39b172e78cf89c82cf51e64e639bb30e677f196a
Andrew Tao <aytao@princeton.edu>
2023-04-24 19:15:06 -0400

    Array creation timing

--- time/time_array_handler.ml
+++ time/time_array_handler.ml
@@ -2,7 +2,13 @@ open Seq
 open Sequence
 open Array_handler
 
-let n = 1000000000
+let n = 1000000
 let memset_init n : int array = get_uninitialized n
-let _, elapsed_time = Time_utils.time memset_init n
-let _ = Printf.printf "%f\n" elapsed_time
+let total_time = ref 0.0;;
+
+for i = 1 to 100 do
+  let _, elapsed_time = Time_utils.time memset_init n in
+  total_time := !total_time +. elapsed_time
+done
+
+let _ = Printf.printf "%f\n" !total_time
--- time/time_array_make.ml
+++ time/time_array_make.ml
@@ -1,4 +1,10 @@
-let n = 1000000000
+let n = 1000000
 let array_init n : int array = Array.make n (-1)
-let _, elapsed_time = Time_utils.time array_init n
-let _ = Printf.printf "%f\n" elapsed_time
+let total_time = ref 0.0;;
+
+for i = 1 to 100 do
+  let _, elapsed_time = Time_utils.time array_init n in
+  total_time := !total_time +. elapsed_time
+done
+
+let _ = Printf.printf "%f\n" !total_time
