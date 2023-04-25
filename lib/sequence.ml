cf4fa9346b6e0e5cb74ece7acdc3e25012d411ad
Andrew Tao <aytao@princeton.edu>
2023-04-25 00:48:42 -0400

    Change up the order of methods. Begin cleaning

--- lib/sequence.ml
+++ lib/sequence.ml
@@ -7,25 +7,25 @@ end
 module type S = sig
   type 'a t
 
-  val tabulate : (int -> 'a) -> int -> 'a t
-  val seq_of_array : 'a array -> 'a t
-  val array_of_seq : 'a t -> 'a array
   val iter : ('a -> unit) -> 'a t -> unit
   val iteri : (int -> 'a -> unit) -> 'a t -> unit
   val length : 'a t -> int
   val empty : unit -> 'a t
-  val cons : 'a -> 'a t -> 'a t
   val singleton : 'a -> 'a t
-  val append : 'a t -> 'a t -> 'a t
   val nth : 'a t -> int -> 'a
-  val map : ('a -> 'b) -> 'a t -> 'b t
-  val map_reduce : ('a -> 'b) -> ('b -> 'b -> 'b) -> 'b -> 'a t -> 'b
-  val reduce : ('a -> 'a -> 'a) -> 'a -> 'a t -> 'a
-  val flatten : 'a t t -> 'a t
+  val cons : 'a -> 'a t -> 'a t
+  val tabulate : (int -> 'a) -> int -> 'a t
   val repeat : 'a -> int -> 'a t
+  val append : 'a t -> 'a t -> 'a t
+  val seq_of_array : 'a array -> 'a t
+  val array_of_seq : 'a t -> 'a array
   val zip : 'a t * 'b t -> ('a * 'b) t
   val split : 'a t -> int -> 'a t * 'a t
+  val map : ('a -> 'b) -> 'a t -> 'b t
+  val reduce : ('a -> 'a -> 'a) -> 'a -> 'a t -> 'a
+  val map_reduce : ('a -> 'b) -> ('b -> 'b -> 'b) -> 'b -> 'a t -> 'b
   val scan : ('a -> 'a -> 'a) -> 'a -> 'a t -> 'a t
+  val flatten : 'a t t -> 'a t
   val filter : ('a -> bool) -> 'a t -> 'a t
 end
 
