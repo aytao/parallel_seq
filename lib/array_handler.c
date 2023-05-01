969460e1c8c16901aaebf31643239db111932b2a
Andrew Tao <aytao@princeton.edu>
2023-04-30 23:37:52 -0400

    Licensing

--- lib/array_handler.c
+++ lib/array_handler.c
@@ -1,3 +1,22 @@
+/**************************************************************************/
+/* Copyright (C) 2023  Andrew Tao                                         */
+/*                                                                        */
+/* This library is free software; you can redistribute it and/or          */
+/* modify it under the terms of the GNU Lesser General Public             */
+/* License as published by the Free Software Foundation; either           */
+/* version 2.1 of the License, or (at your option) any later version.     */
+/*                                                                        */
+/* This library is distributed in the hope that it will be useful,        */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      */
+/* Lesser General Public License for more details.                        */
+/*                                                                        */
+/* You should have received a copy of the GNU Lesser General Public       */
+/* License along with this library; if not, write to the Free Software    */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,             */
+/* MA  02110-1301  USA                                                    */
+/**************************************************************************/
+
 #include <caml/mlvalues.h>
 #include <caml/alloc.h>
 #include <caml/memory.h>
@@ -5,7 +24,7 @@
 
 #define SAFE_VAL -1
 
-/* Adapted from the OCaml runtime Array.make */
+/* Adapted from the OCaml runtime Array.make on 4/24/23 */
 /* [len] is a [value] representing number of words */
 CAMLprim value caml_make_uninitialized_vect(value len)
 {
