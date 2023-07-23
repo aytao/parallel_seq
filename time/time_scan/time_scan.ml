94322a4a2b072991fba342a1809118156f503588
Andrew Tao <aytao@princeton.edu>
2023-07-24 00:43:39 +0100

    array timing

---
+++ time/time_scan/time_scan.ml
@@ -0,0 +1,42 @@
+let ensure_no_pool test_name =
+  if Domainslib.Task.lookup_pool "parallel_seq_pool" |> Option.is_some then
+    failwith ("Test " ^ test_name ^ " must be called with [-f]")
+
+let sequential _seq_mod ?n () =
+  ensure_no_pool "scan_sequential";
+  let n = Option.value ~default:100000 n in
+  let arr = Array.init n (fun x -> x) in
+  Time_test.time
+    (fun () ->
+      let copy = Array.copy arr in
+      for i = 1 to Array.length copy - 1 do
+        copy.(i) <- copy.(i - 1) + copy.(i)
+      done)
+    ()
+
+let copy _seq_mod ?n () =
+  ensure_no_pool "array_copy";
+  let n = Option.value ~default:100000 n in
+  let arr = Array.init n (fun x -> x) in
+  Time_test.time (fun () -> Array.copy arr |> ignore) ()
+
+let domainslib _seq_mod ?n () =
+  ensure_no_pool "scan_domainslib";
+  let n = Option.value ~default:100000 n in
+  let pool =
+    Domainslib.Task.setup_pool
+      ~num_domains:(Parallelseq.Defaults.num_domains_total - 1)
+      ()
+  in
+  let arr = Array.init n (fun x -> string_of_int x) in
+  Time_test.time
+    (fun () ->
+      Domainslib.Task.run pool (fun () ->
+          Domainslib.Task.parallel_scan pool ( ^ ) arr |> ignore))
+    ()
+
+let parallel_seq seq_mod ?n () =
+  let open (val seq_mod : Parallelseq.Sequence.S) in
+  let n = Option.value ~default:100000 n in
+  let s = tabulate (fun x -> string_of_int x) n in
+  Time_test.time (fun () -> scan ( ^ ) "" s |> ignore) ()
---
+++ time/time_scan/time_scan.mli
@@ -0,0 +1,4 @@
+val sequential : Time_test.t
+val domainslib : Time_test.t
+val copy : Time_test.t
+val parallel_seq : Time_test.t
