d2d74746937d8eb03e578b78ac73cdaa203259d9
Andrew Tao <aytao@princeton.edu>
2023-07-24 23:24:09 +0100

    add repeats to run_array_scan_test

--- scripts/run_array_scan_test.py
+++ scripts/run_array_scan_test.py
@@ -1,22 +1,24 @@
 import os
-from sys import argv, exit, stderr
+from sys import argv, exit, stderr, stdout
 
 
 def main():
-  if len(argv) != 3:
-    print("Usage: python", argv[0], "[time name]", "[max num_domains]")
+  if len(argv) != 4:
+    print("Usage: python", argv[0], "[time name]", "[max num_domains]", "[repeats]")
     exit(1)
 
   time_name = argv[1]
   max_num_domains = int(argv[2])
+  repeats = int(argv[3])
 
   command_fmt = "dune exec -- %s -num_domains %d"
+  print(repeats)
   for i in range(1, max_num_domains + 1):
     print("Running '%s' with %d domain(s)..." %
           (time_name, i), file=stderr, end="")
     stderr.flush()
-
-    os.system(command_fmt % (time_name, i))
+    for _ in range(repeats):
+      os.system(command_fmt % (time_name, i))
 
     print("Done", file=stderr)
     stderr.flush()
