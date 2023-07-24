fa18d8aea5b7de3396315a75ff597971d2d4a561
Andrew Tao <aytao@princeton.edu>
2023-07-24 22:13:24 +0100

    Merge maps

--- time/array_init/array_init.mli
+++
@@ -1,3 +0,0 @@
-val sequential : Time_test.t
-val parallel : Time_test.t
-val copy : Time_test.t
--- time/time_scan/time_scan.ml
+++ time/time_scan/time_scan.ml
@@ -20,6 +20,8 @@ let copy _seq_mod ?n () =
   let arr = Array.init n (fun x -> x) in
   Time_test.time (fun () -> Array.copy arr |> ignore) ()
 
+module Int_map = Map.Make (Int)
+
 let domainslib _seq_mod ?n () =
   ensure_no_pool "scan_domainslib";
   let n = Option.value ~default:100000 n in
@@ -28,15 +30,21 @@ let domainslib _seq_mod ?n () =
       ~num_domains:(Parallelseq.Defaults.num_domains_total - 1)
       ()
   in
-  let arr = Array.init n (fun x -> [ x ]) in
+  let arr = Array.init n (fun x -> Int_map.singleton x x) in
   Time_test.time
     (fun () ->
       Domainslib.Task.run pool (fun () ->
-          Domainslib.Task.parallel_scan pool ( @ ) arr |> ignore))
+          Domainslib.Task.parallel_scan pool
+            (Int_map.union (fun x y z -> Some x))
+            arr
+          |> ignore))
     ()
 
 let parallel_seq seq_mod ?n () =
   let open (val seq_mod : Parallelseq.Sequence.S) in
   let n = Option.value ~default:100000 n in
-  let s = tabulate (fun x -> [ x ]) n in
-  Time_test.time (fun () -> scan ( @ ) [] s |> ignore) ()
+  let s = tabulate (fun x -> Int_map.singleton x x) n in
+  Time_test.time
+    (fun () ->
+      scan (Int_map.union (fun x y z -> Some x)) Int_map.empty s |> ignore)
+    ()
