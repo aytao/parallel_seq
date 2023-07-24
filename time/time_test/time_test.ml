9b329909f7b7350b6b7db422166dab29a43d7218
Andrew Tao <aytao@princeton.edu>
2023-07-24 23:03:46 +0100

    automate domainslib scan time test

--- time/time_test/time_test.ml
+++ time/time_test/time_test.ml
@@ -1,6 +1,8 @@
 type config = Parallelseq.Sequence.seq_type * int option
 type t = config -> float
 
+let time_test_pool_name = "time_test_pool"
+
 let time (f : 'a -> 'b) (x : 'a) : float =
   let t = Unix.gettimeofday () in
   let _fx = f x in
--- time/time_test/time_test.mli
+++ time/time_test/time_test.mli
@@ -1,4 +1,5 @@
 type config = Parallelseq.Sequence.seq_type * int option
 type t = config -> float
 
+val time_test_pool_name : string
 val time : ('a -> unit) -> 'a -> float
