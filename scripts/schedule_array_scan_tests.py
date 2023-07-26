c1dbd11464d88990eb2bcb9c7ebce351a5f601dd
Andrew Tao <aytao@princeton.edu>
2023-07-27 00:06:16 +0100

    fix file_path

--- scripts/schedule_array_scan_tests.py
+++ scripts/schedule_array_scan_tests.py
@@ -4,12 +4,12 @@ from sys import argv, exit, stderr, stdout
 
 def main():
   if len(argv) < 4:
-    print("Usage: python", argv[0], "[max num_domains]", "[repeats]", "[time test]...")
+    print("Usage: python", argv[0], "[file_path_prefix]", "[max num_domains]", "[repeats]", "[time test]...")
     exit(1)
 
-  file_path_prefix = argv[0]
-  max_domains = int(argv[1])
-  num_repeats = int(argv[2])
+  file_path_prefix = argv[1]
+  max_domains = int(argv[2])
+  num_repeats = int(argv[3])
 
   def get_command(time_test):
     filename = file_path_prefix + time_test
@@ -23,7 +23,7 @@ def main():
     
     return " ".join(command)
 
-  for time_test in argv[3:]:
+  for time_test in argv[4:]:
     command = get_command(time_test)
 
     print("RUNNING '%s'\n\n" % time_test, file=stderr, end="")
